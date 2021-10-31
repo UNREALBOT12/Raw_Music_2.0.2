47cba5088df1a6dec6802aec6d108d9720836cf9
UNREALBOT12 <91039807+UNREALBOT12@users.noreply.github.com>
2021-10-31 16:02:29 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,23 +10,23 @@ async def start(_, message: Message):
     
     await message.reply_text(
         f"""**
-🌠𝗧𝗵𝗶𝘀 𝗜𝘀 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 \n🌺𝗥𝘂𝗻 𝗢𝗻 𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗩𝗣𝗦 𝗦𝗲𝗿𝘃𝗲𝗿 \n🌼𝗙𝗲𝗲𝗹 𝗛𝗶𝗴𝗵 𝗤𝘂𝗮𝗹𝗶𝘁𝘆 𝗠𝘂𝘀𝗶𝗰 𝗜𝗻 𝗩𝗖 \n⭐𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱 𝗕𝘆 [𝗛𝗲𝘅𝗼𝗿](https://t.me/unreal_hun_bro)**
+🌠𝗧𝗵𝗶𝘀 𝗜𝘀 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 \n🌺𝗥𝘂𝗻 𝗢𝗻 𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗩𝗣𝗦 𝗦𝗲𝗿𝘃𝗲𝗿 \n🌼𝗙𝗲𝗲𝗹 𝗛𝗶𝗴𝗵 𝗤𝘂𝗮𝗹𝗶𝘁𝘆 𝗠𝘂𝘀𝗶𝗰 𝗜𝗻 𝗩𝗖 \n⭐𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱 𝗕𝘆 [ ❰ミ★『ᒪEGEᑎᗪ_ᗯOᖇᒪᗪ_ᗰᑌᔕIᑕ』★彡❱](https://t.me/MAHAKAL_SHIVAY_Legend0)**
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "❰𝗢𝘄𝗻𝗲𝗿❱", url="https://t.me/unreal_hun_bro")
+                        "❰𝗢𝘄𝗻𝗲𝗿❱", url="https://t.me/MAHAKAL_SHIVAY_Legend0")
                   ],[
                     InlineKeyboardButton(
-                        "❰𝗦𝘂𝗽𝗽𝗼𝗿𝘁❱", url="https://t.me/we_are_legendss"
+                        "❰𝗦𝘂𝗽𝗽𝗼𝗿𝘁❱", url="https://t.me/Lowasosiaction"
                     ),
                     InlineKeyboardButton(
-                        "❰𝗚𝗿𝗼𝘂𝗽❱", url="https://t.me/we_are_legendss"
+                        "❰𝗚𝗿𝗼𝘂𝗽❱", url=https://t.me/yaarokimastifrendship""
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "❰𝗦𝗼𝘂𝗿𝗰𝗲❱", url="https://t.me/RAW_X_MUSIC_OFFICIAL"
+                        "❰𝗦𝗼𝘂𝗿𝗰𝗲❱", url="https://github.com/UNREALBOT12/Raw_Music_2.0"
                     )]
             ]
         ),
@@ -35,12 +35,12 @@ async def start(_, message: Message):
 
 @Client.on_message(filters.command("hexor") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 𝗢𝗻𝗹𝗶𝗻𝗲 𝗡𝗼𝘄\n🌠 яαω χ мυѕι¢ <3**""",
+      await message.reply_text("""**𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 𝗢𝗻𝗹𝗶𝗻𝗲 𝗡𝗼𝘄\n🌠<3**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🌼𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/we_are_legendss")
+                        "🌼𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/yaarokimastifrendship")
                 ]
             ]
         )
